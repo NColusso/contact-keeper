6ec5c5680e26c998bd706fc467380b1f8603cc7a
Natasha <natashacolusso@gmail.com>
2021-04-01 13:01:49 -0700

    Feature: when registering will respond back with json web token to be used for logging in after registration

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User.js");
 
@@ -41,7 +42,20 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("User saved");
+      const payload = {
+        user: { id: user.id },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
